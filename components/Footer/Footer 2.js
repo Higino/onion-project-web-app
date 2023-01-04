d4b3fc3d1f467695f78365ce3f7960c5186bbd5b
Higino Silva <higino@creoate.com>
2023-01-04 15:17:49 +0000

    Initial Commit

---
+++ components/Footer/Footer 2.js
@@ -0,0 +1,73 @@
+/*eslint-disable*/
+import React from "react";
+// nodejs library to set properties for components
+import PropTypes from "prop-types";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// material-ui core components
+import { List, ListItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Favorite from "@material-ui/icons/Favorite";
+
+import styles from "/styles/jss/nextjs-material-kit/components/footerStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function Footer(props) {
+  const classes = useStyles();
+  const { whiteFont } = props;
+  const footerClasses = classNames({
+    [classes.footer]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  const aClasses = classNames({
+    [classes.a]: true,
+    [classes.footerWhiteFont]: whiteFont
+  });
+  return (
+    <footer className={footerClasses}>
+      <div className={classes.container}>
+        <div className={classes.left}>
+          <List className={classes.list}>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+                target=""
+              >
+                Media Roots
+              </a>
+            </ListItem>
+            <ListItem className={classes.inlineBlock}>
+              <a
+                href="#"
+                className={classes.block}
+                target=""
+              >
+                About us
+              </a>
+            </ListItem>
+          </List>
+        </div>
+        <div className={classes.right}>
+          &copy; {1900 + new Date().getYear()} , made with{" "}
+          <Favorite className={classes.icon} /> by{" "}
+          <a
+            href="#"
+            className={aClasses}
+            target=""
+          >
+            MediaAssist
+          </a>{" "}
+          for a better social media.
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+Footer.propTypes = {
+  whiteFont: PropTypes.bool
+};
